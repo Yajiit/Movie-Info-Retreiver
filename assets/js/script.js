acbf4e1bb809cb3a852e31e3f487724dce37fcb0
Yajiit <tjagerter@gmail.com>
2023-06-12 18:46:19 -0400

    disabled Watchmode API to prevent hitting limit of 1000

--- assets/js/script.js
+++ assets/js/script.js
@@ -75,53 +75,55 @@ document.addEventListener('DOMContentLoaded', function() {
       });
   }
   
-  // Function to fetch sources using Watchmode API
-  function getSources(titleId) {
-    // puts the imdb titleID into a fetch request
-    fetch(`https://api.watchmode.com/v1/title/${titleId}/details/?apiKey=WPmG7Zz5vHjq40wGf0WSfvoq2z6F38BSqO3r9xPe&append_to_response=sources`)
-    // parses resopnse as JSON object
-      .then(response => response.json())
-      // runs that JSON response object as data
-      .then(data => {
-      // saves JSON response to local storage
-      localStorage.setItem('sources', JSON.stringify(data));
-        console.log(data); // Logs the response data to the console
+// WATCHMODE API FETCH DISABLED FOR TESTING TO SAVE API USAGE LIMIT \\
+
+  // // Function to fetch sources using Watchmode API
+  // function getSources(titleId) {
+  //   // puts the imdb titleID into a fetch request
+  //   fetch(`https://api.watchmode.com/v1/title/${titleId}/details/?apiKey=WPmG7Zz5vHjq40wGf0WSfvoq2z6F38BSqO3r9xPe&append_to_response=sources`)
+  //   // parses resopnse as JSON object
+  //     .then(response => response.json())
+  //     // runs that JSON response object as data
+  //     .then(data => {
+  //     // saves JSON response to local storage
+  //     localStorage.setItem('sources', JSON.stringify(data));
+  //       console.log(data); // Logs the response data to the console
   
-        // if conditional to check that there are more than zero sources fetched
-        if (data.sources && data.sources.length > 0) {
-          // assigns array of streaming sources from data to variable "sources"
-          const sources = data.sources;
-          console.log(sources); // Log the sources to the console
+  //       // if conditional to check that there are more than zero sources fetched
+  //       if (data.sources && data.sources.length > 0) {
+  //         // assigns array of streaming sources from data to variable "sources"
+  //         const sources = data.sources;
+  //         console.log(sources); // Log the sources to the console
   
-          // Remove duplicates from the sources array based on name
-          // extracts all source names into an array with sources.map(source => source.name)
-          // that array passes through "new Set()" to remove duplicate names
-          // then uses Array.from() to create another array and puts the non duplicated names into it
-          const uniqueSources = Array.from(new Set(sources.map(source => source.name)))
+  //         // Remove duplicates from the sources array based on name
+  //         // extracts all source names into an array with sources.map(source => source.name)
+  //         // that array passes through "new Set()" to remove duplicate names
+  //         // then uses Array.from() to create another array and puts the non duplicated names into it
+  //         const uniqueSources = Array.from(new Set(sources.map(source => source.name)))
          
-          // uses sources.find() to invoke a callback function on each element in the sources array
-          // each "name" property of the element "source" is compared to the current name being mapped and returns the element if a match is found
-            .map(name => sources.find(source => source.name === name));
+  //         // uses sources.find() to invoke a callback function on each element in the sources array
+  //         // each "name" property of the element "source" is compared to the current name being mapped and returns the element if a match is found
+  //           .map(name => sources.find(source => source.name === name));
   
-          // grabs the sourcesList div and makes into variable
-          const sourcesList = document.getElementById('sourcesList');
-          // maps an array of each source.name property with list item tags then joins them together into a single string
-          const sourcesHTML = uniqueSources.map(source => `<li>${source.name}</li>`).join('');
-          // takes the list items from sourcesHTML and puts them into a unordered list at the sourcesList <div>
-          sourcesList.innerHTML = `<ul>${sourcesHTML}</ul>`;
-        } else {
-          // if there number of sources is 0, displays "No sources found" message
-          const sourcesList = document.getElementById('sourcesList');
-          sourcesList.innerHTML = `<p>No sources found for this title ID.</p>`;
-        }
-      })
-      // if an error is caught log it to the console and displays error on page
-      .catch(error => {
-        console.log(error);
-        const sourcesList = document.getElementById('sourcesList');
-        sourcesList.innerHTML = `<p>Error: ${error.message}</p>`;
-      });
-  }
+  //         // grabs the sourcesList div and makes into variable
+  //         const sourcesList = document.getElementById('sourcesList');
+  //         // maps an array of each source.name property with list item tags then joins them together into a single string
+  //         const sourcesHTML = uniqueSources.map(source => `<li>${source.name}</li>`).join('');
+  //         // takes the list items from sourcesHTML and puts them into a unordered list at the sourcesList <div>
+  //         sourcesList.innerHTML = `<ul>${sourcesHTML}</ul>`;
+  //       } else {
+  //         // if there number of sources is 0, displays "No sources found" message
+  //         const sourcesList = document.getElementById('sourcesList');
+  //         sourcesList.innerHTML = `<p>No sources found for this title ID.</p>`;
+  //       }
+  //     })
+  //     // if an error is caught log it to the console and displays error on page
+  //     .catch(error => {
+  //       console.log(error);
+  //       const sourcesList = document.getElementById('sourcesList');
+  //       sourcesList.innerHTML = `<p>Error: ${error.message}</p>`;
+  //     });
+  // }
 
   document.addEventListener('DOMContentLoaded', function() {
     // retrieve the movieInfo data from local storage
